f95628015387db7eaa23743db8a913be13f1ce7f
N.I <iworksWeb@gmail.com>
2024-05-03 16:11:53 +0900

    change | cors.php

--- api/config/cors.php
+++ api/config/cors.php
@@ -15,23 +15,7 @@ return [
     |
     */
 
-    // 'paths' => ['api/*', 'sanctum/csrf-cookie'],
-
-    // 'allowed_methods' => ['*'],
-
-    // 'allowed_origins' => ['*'],
-
-    // 'allowed_origins_patterns' => [],
-
-    // 'allowed_headers' => ['*'],
-
-    // 'exposed_headers' => [],
-
-    // 'max_age' => 0,
-
-    // 'supports_credentials' => false,
-
-    'paths' => ['api/*'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['GET', 'POST', 'PUT', 'DELETE'],
     'allowed_origins' => ['*'],
     'allowed_origins_patterns' => [],
